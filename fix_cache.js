733216ee1e36e319486521e69994db5808272ea5
JKiddo-dev <matias.aguilar63@gmail.com>
2026-09-22 01:06:55 -0300

    fix: deshabilitar cache estatica de Next.js

---
+++ fix_cache.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+function walkDir(dir, callback) {
+    fs.readdirSync(dir).forEach(f => {
+        let dirPath = path.join(dir, f);
+        let isDirectory = fs.statSync(dirPath).isDirectory();
+        isDirectory ? walkDir(dirPath, callback) : callback(path.join(dir, f));
+    });
+}
+
+walkDir('/home/jkiddo/tesis-mesh/tesis-frontend/src/app', function(filePath) {
+    if (filePath.endsWith('page.tsx')) {
+        let content = fs.readFileSync(filePath, 'utf8');
+        
+        if (!content.includes("export const dynamic")) {
+            content = "export const dynamic = 'force-dynamic';\n" + content;
+            fs.writeFileSync(filePath, content, 'utf8');
+            console.log('Added force-dynamic to:', filePath);
+        }
+    }
+});
